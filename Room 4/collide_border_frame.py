85674b3169111e90426c85cf7edd5e5dbb0c298c
an-apple-made-of-ash <110041469+an-apple-made-of-ash@users.noreply.github.com>
2024-01-20 21:08:50 +0800

    test

--- Room 4/collide_border_frame.py
+++ Room 4/collide_border_frame.py
@@ -26,7 +26,7 @@ def get_collision_objects(tmx_data, layer_name):
 def main():
     pygame.init()
     screen = pygame.display.set_mode((800, 600))
-    tmx_data = load_pygame(os.path.join(os.path.dirname(__file__),'Room 2.tmx'))
+    tmx_data = load_pygame(os.path.join(os.path.dirname(__file__),'border.tmx'))
     obstacles = get_collision_objects(tmx_data, "Border_layer")
 
     # Player setup
