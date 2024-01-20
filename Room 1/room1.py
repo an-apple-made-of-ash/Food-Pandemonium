cc535850c58aafcad714d26e8a7a1aa6ba114499
Joon Hao <52413727+bjh-developer@users.noreply.github.com>
2024-01-20 12:55:56 +0800

    add concrete floor

---
+++ Room 1/room1.py
@@ -0,0 +1,19 @@
+import os
+import pygame
+from pygame.locals import *
+
+pygame.init()
+screen = pygame.display.set_mode((500, 500), HWSURFACE | DOUBLEBUF | RESIZABLE)
+pic = pygame.image.load("Assets/Pixel_art_grass_image.png")
+screen.blit(pygame.transform.scale(pic, (500, 500)), (0, 0))
+pygame.display.flip()
+while True:
+    pygame.event.pump()
+    event = pygame.event.wait()
+    if event.type == QUIT:
+        pygame.display.quit()
+    elif event.type == VIDEORESIZE:
+        screen = pygame.display.set_mode(
+            event.dict['size'], HWSURFACE | DOUBLEBUF | RESIZABLE)
+        screen.blit(pygame.transform.scale(pic, event.dict['size']), (0, 0))
+        pygame.display.flip()
